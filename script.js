d46a7f549a7b1447606c3953309245e693706701
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-05-03 12:58:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let n = arr.length;
+	let totalCost = 0;
+	while(n>1){
+		arr.sort();
+		let a = arr.splice(0,1);
+		let b = arr.splice(0,1);
+		let cost = a[0]+b[0];
+		totalCost += cost;
+		arr.push(cost);
+		n--;
+	}
+	return totalCost;
   
 }
 
